9aaeee358f0ba507f2403552bf072e99a8eedccd
Alberto Guerra <a.alb.guerra@gmail.com>
2022-09-23 21:12:28 +0200

    [IMPORTANT] HTML+ID to load audio
    Commented how to manage audio files

--- js/audio.js
+++ js/audio.js
@@ -3,7 +3,7 @@ const event = new Event('end');
 var audio = new Audio()
 
 $('.btnAudio').on('click', function () {
-    var src = './assets/sounds/' + this.id + '.mp3'
+    var src = './assets/sounds/' + this.id + '.mp3' /* AUDIO FILE NAME SAME INTO THE HTML AUDIO LOAD */
     audio.src = src
     audio.dispatchEvent(event)
 
