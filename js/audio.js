cf2cef82a37b40415bd34294babec26bb0d0d5b5
Alberto Guerra <a.alb.guerra@gmail.com>
2022-10-07 19:22:05 +0200

    Nicely formatted <3

--- js/audio.js
+++ js/audio.js
@@ -1,37 +1,42 @@
 var isBusy = false;
-const event = new Event('end');
-var audio = new Audio()
+const event = new Event("end");
+var audio = new Audio();
 
-$('.btnAudio').on('click', function () {
-    var src = './assets/sounds/' + this.id + '.mp3' /* AUDIO FILE NAME SAME INTO THE HTML AUDIO LOAD */
-    audio.src = src
-    audio.dispatchEvent(event)
+$(".btnAudio").on("click", function () {
+  var src =
+    "./assets/sounds/" +
+    this.id +
+    ".mp3"; /* AUDIO FILE NAME SAME INTO THE HTML AUDIO LOAD */
+  audio.src = src;
+  audio.dispatchEvent(event);
 
-    isBusy = true;
+  isBusy = true;
 
-    var items = document.getElementsByClassName("audioControl")
-    for (var i = 0; i < items.length; i++) {
-        items[i].style.opacity = 1
-    }
+  var items = document.getElementsByClassName("audioControl");
+  for (var i = 0; i < items.length; i++) {
+    items[i].style.opacity = 1;
+  }
 
-    audio.play()
+  audio.play();
 
-    audio.addEventListener("timeupdate", () => {
-        var seconds = audio.currentTime
-        minutes = Math.floor(seconds / 60);
-        minutes = (minutes >= 10) ? minutes : "0" + minutes;
-        seconds = Math.floor(seconds % 60);
-        seconds = (seconds >= 10) ? seconds : "0" + seconds;
-        document.getElementById("progressAudioTime").innerHTML = `${minutes}:${seconds}`
-        document.getElementById("progressAudioBar").value = audio.currentTime / audio.duration * 100
-    })
+  audio.addEventListener("timeupdate", () => {
+    var seconds = audio.currentTime;
+    minutes = Math.floor(seconds / 60);
+    minutes = minutes >= 10 ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = seconds >= 10 ? seconds : "0" + seconds;
+    document.getElementById(
+      "progressAudioTime"
+    ).innerHTML = `${minutes}:${seconds}`;
+    document.getElementById("progressAudioBar").value =
+      (audio.currentTime / audio.duration) * 100;
+  });
 
-    audio.addEventListener("ended", () => {
-        isBusy = false;
-    })
+  audio.addEventListener("ended", () => {
+    isBusy = false;
+  });
 
-    audio.addEventListener('end', () => {
-        audio.pause();
-    })
-})
-
+  audio.addEventListener("end", () => {
+    audio.pause();
+  });
+});
