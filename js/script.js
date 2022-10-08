31b9e8cbae360bbb7d154542a8d71fa3d479360c
juanalzate29 <jalzat13@gmail.com>
2022-10-08 14:16:22 +0200

    Revert "Merge branch 'main' of https://github.com/juanalzate29/MCSN"
    This reverts commit 8a2ec11526889370e15b00d2606b39372ead5e43, reversing
    changes made to 5d6f76de3f05e0a6d6b3aa710e2163e3230eacd9.

--- js/script.js
+++ js/script.js
@@ -4,33 +4,34 @@
 //
 //––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 
-const track = document.querySelector(".slider_track");
+
+const track = document.querySelector('.slider_track');
 const slides = Array.from(track.children);
-const nextButton = document.querySelector(".slider_button--right");
-const prevButton = document.querySelector(".slider_button--left");
+const nextButton = document.querySelector('.slider_button--right');
+const prevButton = document.querySelector('.slider_button--left');
 const slideWidth = slides[0].getBoundingClientRect().width;
-const indicators = document.querySelector(".slide-indicator");
+const indicators = document.querySelector('.slide-indicator');
 const indicator = Array.from(indicators.children);
-const infoButtonOpen = document.querySelector(".info-button-open");
-const infoButtonClose = document.querySelector(".info-button-close");
-const infoBox = document.querySelector(".info-box");
+const infoButtonOpen = document.querySelector('.info-button-open');
+const infoButtonClose = document.querySelector('.info-button-close');
+const infoBox = document.querySelector('.info-box');
 //console.log(slideWidth);
 
 const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + "px";
+  slide.style.left = slideWidth * index + 'px';
 };
 
 slides.forEach(setSlidePosition);
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
-  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
-  currentSlide.classList.remove("current-slide");
-  targetSlide.classList.add("current-slide");
-};
+  track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+  currentSlide.classList.remove('current-slide');
+  targetSlide.classList.add('current-slide');
+}
 
 // When click Left, move to left
-prevButton.addEventListener("click", (e) => {
-  const currentSlide = track.querySelector(".current-slide");
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
   const prevSlide = currentSlide.previousElementSibling;
   //move to prev slide
   moveToSlide(track, currentSlide, prevSlide);
@@ -38,29 +39,30 @@ prevButton.addEventListener("click", (e) => {
 });
 
 // When click Right, move to right
-nextButton.addEventListener("click", (e) => {
-  const currentSlide = track.querySelector(".current-slide");
+nextButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
   const nextSlide = currentSlide.nextElementSibling;
   //move to next slide
   moveToSlide(track, currentSlide, nextSlide);
   removeArrow();
 });
 
-infoButtonOpen.addEventListener("click", (e) => {
-  infoBox.style.transform = "translateX(-436px)";
-  prevButton.classList.add("is-hidden");
-  nextButton.classList.add("is-hidden");
-});
+infoButtonOpen.addEventListener('click', e => {
+  infoBox.style.transform = 'translateX(-436px)';
+  prevButton.classList.add('is-hidden');
+  nextButton.classList.add('is-hidden');
+})
 
-infoButtonClose.addEventListener("click", (e) => {
-  infoBox.style.transform = "translateX(436px)";
-  prevButton.classList.remove("is-hidden");
-  nextButton.classList.remove("is-hidden");
-});
+infoButtonClose.addEventListener('click', e => {
+  infoBox.style.transform = 'translateX(436px)';
+  prevButton.classList.remove('is-hidden');
+  nextButton.classList.remove('is-hidden');
+  
+})
 
 //Remove Prev and Next Arrows (First and Last)
 const removeArrow = () => {
-  const currentSlide = track.querySelector(".current-slide");
+  const currentSlide = track.querySelector('.current-slide');
   const slideOne = slides[0];
   const slideTwo = slides[1];
   const slideThree = slides[2];
@@ -68,26 +70,29 @@ const removeArrow = () => {
   const indicatorTwo = indicator[1];
   const indicatorThree = indicator[2];
 
+
   if (currentSlide == slideOne) {
-    prevButton.classList.add("is-hidden");
-    indicatorOne.classList.remove("is-hidden");
-    indicatorTwo.classList.add("is-hidden");
-    indicatorThree.classList.add("is-hidden");
+    prevButton.classList.add('is-hidden');
+    indicatorOne.classList.remove('is-hidden');
+    indicatorTwo.classList.add('is-hidden');
+    indicatorThree.classList.add('is-hidden');
     console.log("slide 1");
   } else if (currentSlide == slideTwo) {
-    prevButton.classList.remove("is-hidden");
-    nextButton.classList.remove("is-hidden");
-    indicatorOne.classList.add("is-hidden");
-    indicatorTwo.classList.remove("is-hidden");
-    indicatorThree.classList.add("is-hidden");
+    prevButton.classList.remove('is-hidden');
+    nextButton.classList.remove('is-hidden');
+    indicatorOne.classList.add('is-hidden');
+    indicatorTwo.classList.remove('is-hidden');
+    indicatorThree.classList.add('is-hidden');
     console.log("slide 2");
   } else if (currentSlide == slideThree) {
-    nextButton.classList.add("is-hidden");
-    indicatorOne.classList.add("is-hidden");
-    indicatorTwo.classList.add("is-hidden");
-    indicatorThree.classList.remove("is-hidden");
+    nextButton.classList.add('is-hidden');
+    indicatorOne.classList.add('is-hidden');
+    indicatorTwo.classList.add('is-hidden');
+    indicatorThree.classList.remove('is-hidden');
     console.log("slide 3");
   }
-};
+}
 
 removeArrow();
+
+
