6f1cdc1ab6baf349aeebf0d086e34afae070bf76
juanalzate29 <jalzat13@gmail.com>
2022-10-22 00:15:19 +0200

    preload archivio

--- js/archivio.js
+++ js/archivio.js
@@ -16,3 +16,39 @@ function closeInfo() {
   prev.classList.remove("is-hidden");
   next.classList.remove("is-hidden");
 }
+
+function preload(arrayOfImages) {
+  $(arrayOfImages).each(function(){
+      $(new Image()).src = this;
+
+  });
+}
+
+
+preload([
+  'assets/images/aquila_reale.jpg',
+  'assets/images/arvicola_delle_nevi.jpg',
+  'assets/images/camoscio_F.jpg',
+  'assets/images/camoscio_M.jpg',
+  'assets/images/codirosso_spazzacamino.jpg',
+  'assets/images/codirossone.jpg',
+  'assets/images/coturnice.jpg',
+  'assets/images/culbianco.jpg',
+  'assets/images/fringuello_alpino.jpg',
+  'assets/images/gracchio_alpino.jpg',
+  'assets/images/gracchio_corallino.jpg',
+  'assets/images/lucertola_vivipera.jpg',
+  'assets/images/marasso.jpg',
+  'assets/images/marmotta.jpg',
+  'assets/images/merlo_dal_collare.jpg',
+  'assets/images/passera_scopaiola.jpg',
+  'assets/images/picchio_muraiolo.jpg',
+  'assets/images/salamandra_nera.jpg',
+  'assets/images/sordone.jpg',
+  'assets/images/spioncello.jpg',
+  'assets/images/stambecco_alpino_F.jpg',
+  'assets/images/stambecco_alpino_M.jpg',
+  'assets/images/topo_selvatico_alpino.jpg',
+  'assets/images/toporagno_alpino.jpg'
+]);
+console.log('preloaded!');
