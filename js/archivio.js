ee37afe09316cd94fa3c279f854fa2d0d555972e
juanalzate29 <jalzat13@gmail.com>
2022-12-15 16:06:55 +0100

    Fix archivio table header

--- js/archivio.js
+++ js/archivio.js
@@ -6,7 +6,9 @@ const closeBox = document.querySelector(".close-box");
 function openInfo() {
   document.getElementById("myInfoBox").style.width = "436px";
   document.getElementById("myInfoBox").style.left = "calc(100% - 436px)";
+  document.getElementById("myInfoBox").style.overflow = "auto";
   closeBox.style.width = "calc(100% - 436px)";
+  document.body.style.overflow = "hidden";
   audio.pause();
   for (var i = 0; i < iconImg.length; i++) {
     removeClass();
@@ -19,5 +21,8 @@ function openInfo() {
 function closeInfo() {
   document.getElementById("myInfoBox").style.width = "0";
   document.getElementById("myInfoBox").style.left = "100%";
+  document.getElementById("myInfoBox").style.overflow = "hidden";
   closeBox.style.width = "0";
+  document.body.style.overflow = "auto";
+  
 }
